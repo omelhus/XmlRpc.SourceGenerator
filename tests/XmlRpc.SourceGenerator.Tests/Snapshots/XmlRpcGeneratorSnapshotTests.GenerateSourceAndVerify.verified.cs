b8c681cef28d6b390dd2b3f32b347732a8420324
Ole Melhus <ole@on-it.no>
2022-03-25 17:08:34 +0100

    updated tests

--- tests/XmlRpc.SourceGenerator.Tests/Snapshots/XmlRpcGeneratorSnapshotTests.GenerateSourceAndVerify.verified.cs
+++ tests/XmlRpc.SourceGenerator.Tests/Snapshots/XmlRpcGeneratorSnapshotTests.GenerateSourceAndVerify.verified.cs
@@ -1,40 +1,39 @@
 ﻿//HintName: TestXmlRpcClient.g.cs
 using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.DependencyInjection;
+using XmlRpc.SourceGenerator;
 namespace TestSourceGenNamespace;
 
-public class TestXmlRpcClientOptions
-{
-    public string Url { get; set; }
+public class TestXmlRpcClientOptions {
+  public string Url { get; set; }
 }
 
-public static class TestXmlRpcClientServiceExtensions
-{
-    public static IServiceCollection AddTestXmlRpcClient(this IServiceCollection serviceCollection, Action<TestXmlRpcClientOptions> configureTestXmlRpcClient = null)
-    {
-        serviceCollection.AddHttpClient();
-        serviceCollection.AddTransient<ITestXmlRpcClient, TestXmlRpcClient>();
-        if (configureTestXmlRpcClient != null)
+public static class TestXmlRpcClientServiceExtensions {
+public static IServiceCollection AddTestXmlRpcClient(this IServiceCollection serviceCollection, Action<TestXmlRpcClientOptions> configureTestXmlRpcClient = null){
+       serviceCollection.AddHttpClient();
+       serviceCollection.AddTransient<ITestXmlRpcClient, TestXmlRpcClient>();
+       if(configureTestXmlRpcClient != null)
             serviceCollection.Configure<TestXmlRpcClientOptions>(configureTestXmlRpcClient);
-        return serviceCollection;
+       return serviceCollection;
     }
 }
 
-public partial class TestXmlRpcClient : ITestXmlRpcClient
-{
+public partial class TestXmlRpcClient : ITestXmlRpcClient {
 
     private readonly System.Net.Http.HttpClient httpClient;
     public string Url { get; set; }
-    public TestXmlRpcClient(System.Net.Http.HttpClient httpClient, IOptions<TestXmlRpcClientOptions> options)
-    {
+    public TestXmlRpcClient(System.Net.Http.HttpClient httpClient, IOptions<TestXmlRpcClientOptions> options){
         this.httpClient = httpClient;
         Url = options?.Value?.Url;
     }
     public virtual async Task<string> TestConnect()
-    {
-        return await httpClient.SendXmlRpcRequest<string>(Url, "IntegrationManager.testConnect").ConfigureAwait(false);
-    }
+	{
+		return await httpClient.SendXmlRpcRequest<string>(Url, "IntegrationManager.testConnect").ConfigureAwait(false);
+	}
     public virtual async Task<string> GetWorkshops(string sysnme, string syspwd)
-    {
-        return await httpClient.SendXmlRpcRequest<string>(Url, "IntegrationManager.getWorkshops", sysnme, syspwd).ConfigureAwait(false);
-    }
+	{
+		return await httpClient.SendXmlRpcRequest<string>(Url, "IntegrationManager.getWorkshops", sysnme, syspwd).ConfigureAwait(false);
+	}
 }
