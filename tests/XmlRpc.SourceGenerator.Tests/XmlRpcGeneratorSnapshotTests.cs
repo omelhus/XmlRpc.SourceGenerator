77d17a11f178cb9dc922e59bc8d65d65ecde7e56
Ole Melhus <ole@on-it.no>
2022-03-29 11:33:32 +0200

    add Microsoft.SourceLink.GitHub

--- tests/XmlRpc.SourceGenerator.Tests/XmlRpcGeneratorSnapshotTests.cs
+++ tests/XmlRpc.SourceGenerator.Tests/XmlRpcGeneratorSnapshotTests.cs
@@ -92,6 +92,34 @@ public interface ITestXmlRpcClient
         }
 
         [TestMethod]
+        public Task GenerateSourceWithSummaryComments()
+        {
+            var source = @"
+using XmlRpc.SourceGenerator;
+
+namespace TestSourceGenNamespace;
+
+[XmlRpcClient(""IntegrationManager"")]
+public interface ITestXmlRpcClient
+{
+    /// <summary>
+    /// Function for populating suppliers product registry with data.
+    /// Note1! This function will not update the registry, but overwrite it.It will clear all previous products and then insert all products that are submitted in the file.
+    /// Note2! The function will only trigger if the submitted productfile is 100% correct.If one or more errors are detected, the import is aborted and previous registry content is left intact.
+    /// Note3! The csv file format used is the same as when uploading products through the web interface.
+    /// </summary>
+    /// <param name=""accesskey"">Access key for remote system validation</param>
+    /// <param name=""supnme"">Suppliers username</param>
+    /// <param name=""suppwd"">Suppliers password</param>
+    /// <param name=""file"">Csv productfile filecontent as byte array</param>
+    /// <returns>Xml message describing the result of the import</returns>
+    Task<string> uploadProducts(String accesskey, String supnme, String suppwd, byte[] file);
+}";
+            return Verify(TestHelper.Compile(source))
+                         .UseDirectory("Snapshots");
+        }
+
+        [TestMethod]
         public Task InvalidReturnTypeInSource()
         {
             var source = @"
